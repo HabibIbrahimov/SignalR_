316b300f16cd9035a487a577abe2a217b8f5c8d3
HabibIbrahimov <habibyi@code.edu.az>
2022-02-14 22:33:15 +0400

    feat:created signalR

---
+++ SingnaIR/DAL/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SingnaIR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingnaIR.DAL
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
